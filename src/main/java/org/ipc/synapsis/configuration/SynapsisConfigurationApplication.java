31e7a190b1e9832466fd58951bd77f3d1680cafe
mbasri <basrimohamed@live.fr>
2016-07-12 23:09:33 +0200

    [CONFIG]Change package

--- src/main/java/org/ipc/synapsis/configuration/SynapsisConfigurationApplication.java
+++ src/main/java/org/ipc/synapsis/configuration/SynapsisConfigurationApplication.java
@@ -1,4 +1,4 @@
-package org.ipc.synapsis;
+package org.ipc.synapsis.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
